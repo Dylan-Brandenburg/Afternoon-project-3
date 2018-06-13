5bc6ae37a38a75a4e0e12a60fc17710ab2c457eb
Dylan Brandenburg <brandenburgd@Gmail.com>
2018-06-13 12:32:18 -0500

    finished question 2

--- afternoonsql.sql
+++ afternoonsql.sql
@@ -1,8 +1,17 @@
 create table person (
   ID integer primary key autoincrement,
-  Name varChar(512),
+  Name varChar
+(512),
   Age integer,
   Height integers,
   City string,
   FavoriteColor string
   );
+insert into person
+    (name, age, height, city, favoritecolor)
+Values
+    ('Dylan', 26, 180, "Dallas", "Blue"),
+    ('Otis', 9, 45, "Dallas", "Green"),
+    ('Leslie', 27, 150, "Dallas", "Pink"),
+    ('Erich', 29, 180, 'Dallas', "Yellow"),
+    ('Ashlynn', 24 , 160, 'Plano', "purple")
